11d0e91760b97311aa66e319bb3425ab408c407b
Falaloon <sr.ratchatapusit@gmail.com>
2023-11-12 11:23:42 +0700

    update cor oringin server to vercel

--- Server.js
+++ Server.js
@@ -7,13 +7,15 @@ const cors = require("cors");
 const bodyParse = require("body-parser");
 const connectDB = require("./config/db");
 
+
 const app = express();
 //connect db
 connectDB();
 
 //middleware
 app.use(morgan("dev"));
-app.use(cors());
+app.use(cors({origin: ["https://book-club-night-reviews-fontend.vercel.app/"],
+credentials: true,}));
 app.use(bodyParse.json({ limit: "10mb" }));
 
 readdirSync("./Routers").map((r) => {
--- config/db.js
+++ config/db.js
@@ -4,6 +4,7 @@ const connectDB = async () => {
   try {
     await mongoose.connect(
       "mongodb+srv://admin:adminmongo@cluster0.txkyaei.mongodb.net/roifon"
+      // process.env.Database
     );
     console.log("DB connected");
   } catch (error) {
